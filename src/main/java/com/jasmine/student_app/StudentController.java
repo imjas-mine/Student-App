73ee9bf8e1099a7dc68ccd39e5b77d55a4d628f6
jasmine <116243241+imjas-mine@users.noreply.github.com>
2025-09-20 11:33:34 -0400

    running multiple containers on the same network

--- src/main/java/com/jasmine/student_app/StudentController.java
+++ src/main/java/com/jasmine/student_app/StudentController.java
@@ -2,6 +2,7 @@ package com.jasmine.student_app;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,18 @@ import java.util.List;
 public class StudentController {
     @Autowired
     StudentRepo repo;
-    @RequestMapping("/getStudents")
+    @GetMapping("/getStudents")
     public List<Student> getStudents(){
         return repo.findAll();
+    }
+
+    @RequestMapping("/addStudent")
+    public void addStudent(){
+
+        Student s = new Student();
+        s.setName("Jasmine");
+        s.SetAge(22);
+
+        repo.save(s);
     }
 }
